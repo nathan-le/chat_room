fef2c11d8985fa4453452a3f5d554be0ab5e8e3a
nathan-le <nathan.le@berkeley.edu>
2017-05-25 11:08:15 -0700

    finished project

--- chat_client.py
+++ chat_client.py
@@ -26,25 +26,29 @@ class ChatClient(object):
     #Citation: Referenced and modified https://github.com/keyan/python-socket-chat
     def sendMessage(self):
         while True:
-            socket_list = [sys.stdin, self.socket]
-            read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+            try:
+                socket_list = [sys.stdin, self.socket]
+                read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
         
-            for sock in read_sockets:
-                #incoming messages
-                if sock == self.socket:
-                    data = sock.recv(4096)
-                    if not data:
-                        print CLIENT_CANNOT_CONNECT
-                        sys.exit()
+                for sock in read_sockets:
+                    #incoming messages
+                    if sock == self.socket:
+                        data = sock.recv(4096)
+                        if not data:
+                            print CLIENT_CANNOT_CONNECT
+                            sys.exit()
+                        else:
+                            sys.stdout.write(data)
+                            self.prompt()
+                    #outgoing messages    
                     else:
-                        sys.stdout.write(data)
+                        msg = sys.stdin.readline()
+                        self.socket.send(msg)
                         self.prompt()
+            except KeyboardInterrupt:
+                msg = SERVER_CLIENT_LEFT_CHANNEL
+                self.socket.send(msg)
 
-                #outgoing messages    
-                else:
-                    msg = sys.stdin.readline()
-                    self.socket.send(msg)
-                    self.prompt()
 
 
 if __name__ == "__main__":
--- chat_server.py
+++ chat_server.py
@@ -7,7 +7,7 @@ class ChatServer(object):
 
     def __init__(self, port):
         self.port = int(port)
-        self.users = {} #list of clients on this server
+        self.users = {} #dictionary of key=name to value=channel
         self.channels = {} #dictionary of key=channels to value=list of username
         self.addr = {} #dictionary of key=addr to value=username
         self.connection_list = [] #list of users' sockets
@@ -16,7 +16,6 @@ class ChatServer(object):
     def sendToAll(self, sock, message, channel):
         for socket in self.connection_list:
             #if the socket is not the server or the client from which the message originated
-            
             if socket != self.socket and socket != sock:
                 name = self.addr[socket.getpeername()[1]]
                 if name in channel:
@@ -28,14 +27,6 @@ class ChatServer(object):
                         self.connection_list.remove(socket)
                         channel.remove(name)
 
-    # def inChannel(self, name, con):
-    #     for chan in self.channels.keys():
-    #         if name in chan:
-    #             return True
-
-    #     con.send(SERVER_CLIENT_NOT_IN_CHANNEL)
-    #     return False
-
 
     def switch(self, user, index):
         for channel in self.channels:
@@ -46,17 +37,30 @@ class ChatServer(object):
 
     def checkCreate(self, chname, con):
         for check in self.channels.keys():
-            if chname in check:
-                con.send(SERVER_CHANNEL_EXISTS)
+            if chname == check:
+                con.send('\n' + SERVER_CHANNEL_EXISTS.format(chname) + '\n')
                 return False
         return True
 
     def checkJoin(self, chname, con):
         for check in self.channels.keys():
-            if chname in check:
+            if chname == check:
                 return True
-        con.send(SERVER_NO_CHANNEL_EXISTS)
+        con.send('\n' + SERVER_NO_CHANNEL_EXISTS.format(chname) + '\n')
         return False
+
+    def removeUser(self, sock):
+        try:
+            channel = None
+
+            for ch in self.channels.keys():
+                if user in self.channels[ch]:
+                    channel = ch
+            # self.channels.pop(user, None)
+            # self.channels.pop(addr, None)
+            self.channels[channel].remove(user)
+        except:
+            return
 
     def start(self):
         self.socket.bind(("0.0.0.0", self.port))
@@ -66,7 +70,6 @@ class ChatServer(object):
 
         while True:
             read_sockets, write_sockets, error_sockets = select.select(self.connection_list, [], [])
-        
             for sock in read_sockets:
                 #new connection
                 if sock == self.socket:
@@ -90,25 +93,34 @@ class ChatServer(object):
                                     self.channels[data[8:]] = []
                                     self.switch(name, data[8:])
                                     self.users[name] = data[8:]
+                                    self.sendToAll(sock, 
+                                        '\n'+SERVER_CLIENT_JOINED_CHANNEL.format(name)+'\n', 
+                                        self.channels[self.users[name]])
+
 
                             elif '/join' == data[0:5]:
                                 if self.checkJoin(data[6:], sock):
                                     self.switch(name, data[6:])
                                     self.users[name] = data[6:]
+                                    self.sendToAll(sock, 
+                                        '\n'+SERVER_CLIENT_JOINED_CHANNEL.format(name)+'\n', 
+                                        self.channels[self.users[name]])
 
                             elif '/list' == data[0:5]:
                                 sock.send("\n%s\n" % self.channels.keys())
-
                             else:
                                 if self.users[name]:
                                     self.sendToAll(sock, 
                                         "\r" + '[' + self.addr[user_addr] + '] ' + data, 
                                         self.channels[self.users[name]])
                                 else:
-                                    sock.send(SERVER_CLIENT_NOT_IN_CHANNEL)
+                                    sock.send('\n' + SERVER_CLIENT_NOT_IN_CHANNEL + '\n')
                     
                     except:
-                        # self.sendToAll(sock, "\n%s is offline\n" % self.addr[addr[1]])
+                        self.sendToAll(sock,
+                            '\n'+SERVER_CLIENT_LEFT_CHANNEL.format(name)+'\n', 
+                            self.channels[self.users[name]])
+
                         sock.close()
                         self.connection_list.remove(sock)
                         continue
